40e2967daee9f4e0a3f923acba80188973d9b982
uiGalaxyGalaxy <uigalaxyandromeda@gmail.com>
2026-08-11 23:37:33 -0400

    verefl slop

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,10 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
     preprocess: vitePreprocess(),
     kit: {
         adapter: adapter({
-            pages: 'build',
-            assets: 'build',
-            fallback: undefined,
-            precompress: false,
-            strict: true
         }),
         paths: {
             base: '/starry'
