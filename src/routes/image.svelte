8ac1535b3efec95e61790f40466589170b872f7e
uiGalaxyGalaxy <uigalaxyandromeda@gmail.com>
2026-08-11 23:44:03 -0400

    verefl slop

--- src/routes/image.svelte
+++ src/routes/image.svelte
@@ -2,5 +2,5 @@
     let props = $props();
 </script>
 
-<img src={`src/lib/assets/${props.link}`} class="h-64 mt-1 justify-self-center" alt="Robot" />
+<img src={`${props.link}`} class="h-64 mt-1 justify-self-center" alt="Robot" />
 <div class="italic justify-self-center mt-1 opacity-80">{props.text}</div>
